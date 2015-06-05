2834d2f78b1240348364e0232ebd0f4793173d1e
aburkhalter512 <aburkhalter512@gmail.com>
2015-06-04 21:15:38 -0700

    The untested version of FileTable.java is complete. Also added in the Directory class.

--- FileTable.java
+++ FileTable.java
@@ -13,10 +13,80 @@ public class FileTable
 	
 	public synchronized FileTableEntry falloc(String filename, String mode)
 	{
+		bool isNewEntry = false;
+		
+			
+		Inode inode;
+		
+		int iNumber = dir.namei(filename);
+		if (iNumber == -1) // If the inode does not exist in the filesystem
+		{
+			iNumber = dir.ialloc(filename);
+			
+			if (iNumber == -1) // If inode allocation failed
+				return null;
+				
+			Inode inode = new Inode(iNumber);
+			inode.count = 1;
+			
+			isNewEntry = true;
+		}
+		else // If the directory knows about the file
+		{
+			int tableIndex = -1;
+			for (int i = 0; i < table.size(); i++)
+			{
+				if (table[i].iNumber == iNumber)
+				{
+					tableIndex = i;
+					break;
+				}
+			}
+			
+			if (tableIndex == -1) // If the file has not been opened yet
+			{
+				Inode inode = new Inode(iNumber);
+				inode.count = 1;
+				
+				isNewEntry = true;
+			}
+			else // If the file is opened and we store a reference of it in our table
+			{
+				Inode inode = table[i].inode;
+				inode.count++;
+			}
+		}
+		
+		inode.toDisk(iNumber);
+				
+		FileTableEntry entry = new FileTableEntry(inode, iNumber, mode);
+		entry.count = 1;
+		
+		if (isNewEntry)
+			table.add(entry);
+			
+		return entry;
 	}
 	
 	public synchronized boolean ffree(FileTableEntry e)
 	{
+		int tableIndex = -1;
+		for (int i = 0; i < table.size(); i++)
+		{
+			if (table[i].iNumber == e.iNumber)
+			{
+				tableIndex = i;
+				break;
+			}
+		}
+		
+		if (tableIndex == -1) // If the FileTableEntry was not created by us
+			return false;
+			
+		FileTableEntry entry = table.remove(tableIndex);
+		entry.inode.toDisk(entry.iNumber);
+		
+		return true;
 	}
 	
 	public synchronized boolean fempty()
