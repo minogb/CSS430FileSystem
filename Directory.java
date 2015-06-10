3e9ac4810ff6cc0d73d986dac634a632ea73eb04
minogb <minoguebrad@rocketmail.com>
2015-06-10 04:07:19 -0700

    More comments!

--- Directory.java
+++ Directory.java
@@ -1,173 +1,225 @@
-public class Directory
+
+/**
+ * This class contains and manages the file directory
+ */
+public class Directory 
 {
-	private static int maxChars = 30; // max characters of each file name
- 
-	// Directory entries
-	public int fsize[];        // each element stores a different file name size.
-	public char fnames[][];    // each element stores a different file name.
- 
-	public Directory( int maxInumber ) { // directory constructor
-		fsize = new int[maxInumber];     // maxInumber = max files
-		fnames = new char[maxInumber][maxChars];
-		for ( int i = 0; i < maxInumber; i++ )
-		{
-			fsize[i] = 0;                 // all file size initialized to 0
-			fnames[i][0] = 0;
-		}
-		
-		String root = "/";                // entry(inode) 0 is "/"
-		fsize[0] = root.length( );        // fsize[0] is the size of "/".
-		root.getChars( 0, fsize[0], fnames[0], 0 ); // fnames[0] includes "/"
-	}
- 
-	public int bytes2directory( byte[] data )
-	{
-		if (data == null || data.length == 0)
-			return -1;
-	
-		int offset = 0;
-		int maxInumber = SysLib.bytes2int(data, offset);
-		if (maxInumber < 1)
-			return -1;
-			
-		offset = 4;
-		
-		fsize = new int[maxInumber];
-		fnames = new char[maxInumber][maxChars];
-		
-		int iNumber = 0;
-		while (iNumber < maxInumber)
-		{
-			fsize[iNumber] = (char)SysLib.bytes2int(data, offset);
-			offset += 4;
-			
-			for (int i = 0; i < fsize[iNumber]; i++)
-				fnames[iNumber][i] = (char)data[offset++];
-				
-			iNumber++;
-		}
-		
-		return maxInumber;
-	}
- 
-	public byte[] directory2bytes( )
-	{
-		int totalFSize = 0;
-		for (int i = 0; i < fsize.length; i++)
-			totalFSize += fsize[i];
-			
-		byte[] serialized = new byte[4 + fsize.length * 4 + totalFSize];
-		
-		int index = 0;
-		
-		SysLib.int2bytes(fsize.length, serialized, index);
-		index = 4;
-		
-		for (int i = 0; i < fsize.length; i++)
-		{
-			SysLib.int2bytes(fsize[i], serialized, index);
-			index += 4;
-		
-			for (int j = 0; j < fsize[i]; j++)
-				serialized[index++] = (byte) fnames[i][j];
-			
-		}
-		
-		return serialized;
-	}
- 
-	public short ialloc(String filename)
-	{
-		SysLib.cerr("short ialloc(String filename)\n");
-	
-		if (filename.length() > maxChars)
-			return -1;
-		
-		short iNumber = -1;
-		short freeINumber= -1;
-		
-		// Find the first free space or determine if the file exists
-		for (iNumber = 0; iNumber < fsize.length; iNumber++)
-		{
-			String existingFName = new String(fnames[iNumber]);
-			
-			if (fnames[iNumber][0] == 0 && freeINumber == -1)
-				freeINumber = iNumber;
-				
-			if (compare(filename, fnames[iNumber]) == 0)
-				break;
-		}
-		
-		SysLib.cerr("  iNumber: " + iNumber + ", freeINumber: " + freeINumber + "\n");
-		
-		// If no free space was found or the file already exists
-		if (freeINumber == -1 || iNumber < fsize.length)
-			return -1;
-			
-		fsize[freeINumber] = filename.length();
-		filename.getChars(0, fsize[freeINumber], fnames[freeINumber], 0);
-		short val = (short) (freeINumber + 1);
-		Inode inode = new Inode(val);
-		inode.invalidate();
-		inode.toDisk(val);
-	
-		SysLib.cerr("returning ialloc()");
-	
-		return val;
-	}
- 
-	public boolean ifree( short iNumber )
-	{
-		if (iNumber < 1 || iNumber > fsize.length || fnames[iNumber - 1] == null)
-			return false;
-		
-		Inode inode = new Inode(iNumber);
-		inode.invalidate();
-		inode.toDisk(iNumber);
-		
-		fsize[iNumber - 1] = 0;
-		fnames[iNumber - 1][0] = 0;
-		
-		return true;
-	}
- 
-	public short namei( String filename )
-	{
-		if (filename.length() > maxChars)
-			return -1;
-		
-		short iNumber;
-		for (iNumber = 0; iNumber < fsize.length; iNumber++)
-		{
-			if (compare(filename, fnames[iNumber]) == 0)
-				break;
-		}
-		
-		return (short) ((iNumber < fsize.length) ? iNumber + 1 : -1);
-	}
-	
-	public String iname(short iNumber)
-	{
-		if (iNumber < 1 || iNumber >= fsize.length)
-			return null;
-			
-		return new String(fnames[iNumber]);
-	}
-	
-	public static byte compare(String str, char[] cStr)
-	{
-		char[] converted = new char[cStr.length];
-		str.getChars(0, (str.length() > converted.length) ? converted.length : str.length(), converted, 0);
-		
-		for (int i = 0; i < converted.length; i++)
-		{
-			if (converted[i] < cStr[i])
-				return -1;
-			else if (converted[i] > cStr[i])
-				return 1;
-		}
-		
-		return 0;
-		
-	}
+    /**
+     * max characters of each file name
+     */
+    private static int maxChars = 30;
+
+    // Directory entries
+    /**
+     * contains the file name length of each entry
+     */
+    public int fsize[];
+    /**
+     * each file name entry
+     */
+    public char fnames[][];
+    /**
+     * creates a directory with a max num of files
+     * @param maxInumber 
+     */
+    public Directory(int maxInumber) {
+        //init 
+        fsize = new int[maxInumber];
+        fnames = new char[maxInumber][maxChars];
+        //zero out data
+        for (int i = 0; i < maxInumber; i++) {
+            fsize[i] = 0;
+            fnames[i][0] = 0;
+        }
+        //set the root of the directory
+        String root = "/";
+        // fsize[0] is the size of "/".
+        fsize[0] = root.length();      
+        // fnames[0] includes "/"
+        root.getChars(0, fsize[0], fnames[0], 0); 
+    }
+    /**
+     * convert the data to the directory
+     * @param data containing directory info
+     * @return size of directory/error
+     */
+    public int bytes2directory(byte[] data) {
+        //validate
+        if (data == null || data.length == 0) {
+            return -1;
+        }
+
+        int offset = 0;
+        int maxInumber = SysLib.bytes2int(data, offset);
+        //validate
+        if (maxInumber < 1) {
+            return -1;
+        }
+        //offset starts at 4, size of int
+        offset = 4;
+        //init
+        fsize = new int[maxInumber];
+        fnames = new char[maxInumber][maxChars];
+
+        int iNumber = 0;
+        //load the data to the directory
+        while (iNumber < maxInumber) {
+            fsize[iNumber] = (char) SysLib.bytes2int(data, offset);
+            offset += 4;
+
+            for (int i = 0; i < fsize[iNumber]; i++) {
+                fnames[iNumber][i] = (char) data[offset++];
+            }
+
+            iNumber++;
+        }
+
+        return maxInumber;
+    }
+    /**
+     * @return the current directory as a byte[]
+     */
+    public byte[] directory2bytes() {
+        int totalFSize = 0;
+        for (int i = 0; i < fsize.length; i++) {
+            totalFSize += fsize[i];
+        }
+
+        byte[] serialized = new byte[4 + fsize.length * 4 + totalFSize];
+
+        int index = 0;
+
+        SysLib.int2bytes(fsize.length, serialized, index);
+        index = 4;
+
+        for (int i = 0; i < fsize.length; i++) {
+            SysLib.int2bytes(fsize[i], serialized, index);
+            index += 4;
+
+            for (int j = 0; j < fsize[i]; j++) {
+                serialized[index++] = (byte) fnames[i][j];
+            }
+
+        }
+
+        return serialized;
+    }
+    /**
+     * allocate a new dir with an filename
+     * @param filename the new file
+     * @return success/ number
+     */
+    public short ialloc(String filename) {
+        //validate
+        if (filename.length() > maxChars) {
+            return -1;
+        }
+
+        short iNumber = -1;
+        short freeINumber = -1;
+
+        // Find the first free space or determine if the file exists
+        for (iNumber = 0; iNumber < fsize.length; iNumber++) {
+            String existingFName = new String(fnames[iNumber]);
+
+            if (fnames[iNumber][0] == 0 && freeINumber == -1) {
+                freeINumber = iNumber;
+            }
+
+            if (compare(filename, fnames[iNumber]) == 0) {
+                break;
+            }
+        }
+
+        // If no free space was found or the file already exists
+        if (freeINumber == -1 || iNumber < fsize.length) {
+            return -1;
+        }
+
+        fsize[freeINumber] = filename.length();
+        filename.getChars(0, fsize[freeINumber], fnames[freeINumber], 0);
+        short val = (short) (freeINumber + 1);
+        Inode inode = new Inode(val);
+        inode.invalidate();
+        inode.toDisk(val);
+
+        return val;
+    }
+    /**
+     * Free a dir based on inumber
+     * @param iNumber number of inode
+     * @return success/error
+     */
+    public boolean ifree(short iNumber) {
+        //validate
+        if (iNumber < 1 || iNumber > fsize.length || fnames[iNumber - 1] == null) {
+            return false;
+        }
+        //load info of inode from memory
+        Inode inode = new Inode(iNumber);
+        //invalidate it
+        inode.invalidate();
+        //store the new invalidated inode to memory
+        inode.toDisk(iNumber);
+        //zero out its info in dir
+        fsize[iNumber - 1] = 0;
+        fnames[iNumber - 1][0] = 0;
+
+        return true;
+    }
+    /**
+     * get the inumber of a file
+     * @param filename to lookup the inumber
+     * @return the inumber
+     */
+    public short namei(String filename) {
+        //validate
+        if (filename.length() > maxChars) {
+            return -1;
+        }
+
+        short iNumber;
+        //lookup
+        for (iNumber = 0; iNumber < fsize.length; iNumber++) {
+            if (compare(filename, fnames[iNumber]) == 0) {
+                break;
+            }
+        }
+        //return number
+        return (short) ((iNumber < fsize.length) ? iNumber + 1 : -1);
+    }
+    /**
+     * get the filename based on the inumber
+     * @param iNumber number of the inode
+     * @return the name of the file
+     */
+    public String iname(short iNumber) {
+        //validate
+        if (iNumber < 1 || iNumber >= fsize.length) {
+            return null;
+        }
+        //the filename
+        return new String(fnames[iNumber]);
+    }
+    /**
+     * we have had issues comparing strings/chars. this function does that
+     * @param str string to compare with
+     * @param cStr char string to compare to
+     * @return false/true
+     */
+    public static byte compare(String str, char[] cStr) {
+        char[] converted = new char[cStr.length];
+        str.getChars(0, (str.length() > converted.length) ? converted.length : str.length(), converted, 0);
+
+        for (int i = 0; i < converted.length; i++) {
+            if (converted[i] < cStr[i]) {
+                return -1;
+            } else if (converted[i] > cStr[i]) {
+                return 1;
+            }
+        }
+
+        return 0;
+
+    }
 }
--- SuperBlock.java
+++ SuperBlock.java
@@ -1,179 +1,218 @@
-class SuperBlock
+
+/**
+ * Superblock manages all blocks, not files or inodes
+ */
+class SuperBlock 
 {
-	public static int totalBlocks; // the number of disk blocks
-	public static int totalInodes; // the number of inodes
-	public static int freeList;    // the block number of the free list's head
-        //default sugested by faq on assignment
-        public static final int DEFAULT_INODES = 64;
-        public static final int INODES_PER_BLOCK = 16;
-        //default number of blocks spec by faq/assignment
-        public static final int DEFAULT_NUM_BLOCKS = 1000;
-		
-		private static final short NO_MORE_BLOCKS = -1;
-		
-        public SuperBlock(int diskSize)
-        {
-            byte[] block = new byte[Disk.blockSize];
-            if (SysLib.rawread(0, block) < 0)
-            {
-                    invalidate();
-                    return;
-            }
-            totalBlocks = SysLib.bytes2int(block, 0);
-            totalInodes = SysLib.bytes2int(block, 4);
-            freeList = SysLib.bytes2int(block, 8);
-            //checks to see if the disk is already formated
-            if(totalBlocks == diskSize && totalInodes > 0 && freeList > 1)
-            {
-                //it is, do nothing
-            }
-            else //otherwise we need to start to format
-            {
-                //fix any missmatch
-                totalBlocks = diskSize;
-				totalInodes = DEFAULT_INODES;
-				freeList = totalInodes / INODES_PER_BLOCK + 1;
-            }
-			
-			SysLib.int2bytes(totalBlocks, block, 0);
-			SysLib.int2bytes(totalInodes, block, 4);
-			SysLib.int2bytes(freeList, block, 8);
-			
-			SysLib.rawwrite(0, block);
+    //public statics
+    /**
+     * The total number of blocks this system has access to
+     */
+    public static int totalBlocks;
+    /**
+     * the total number of inodes this system can have access to
+     */
+    public static int totalInodes;
+    /**
+     * pointer to the first free block
+     */
+    public static int freeList;   
+    /**
+     * default number of inodes sugested by faq on assignment
+     */
+    public static final int DEFAULT_INODES = 64;
+    /**
+     * the number of inodes one block can contain
+     */
+    public static final int INODES_PER_BLOCK = 16;
+    /**
+     * default number of blocks spec by faq/assignment
+     */
+    public static final int DEFAULT_NUM_BLOCKS = 1000;
+
+    //private statics 
+    /**
+     * value that is used for returns when we are out of blocks
+     */
+    private static final short NO_MORE_BLOCKS = -1;
+    /**
+     * Create a superblock for a filesystem
+     * @param diskSize the size of our disk
+     */
+    public SuperBlock(int diskSize) {
+        byte[] block = new byte[Disk.blockSize];
+        //if invalid, invalidate superblock block
+        if (SysLib.rawread(0, block) < 0) {
+            invalidate();
+            return;
         }
-		
-        public void invalidate()
+        //save information to a byte[] tosave to disk
+        totalBlocks = SysLib.bytes2int(block, 0);
+        totalInodes = SysLib.bytes2int(block, 4);
+        freeList = SysLib.bytes2int(block, 8);
+        //checks to see if the disk is already formated
+        if (totalBlocks == diskSize && totalInodes > 0 && freeList > 1) {
+            //it is, do nothing
+        } else //otherwise we need to start to format
         {
-            totalBlocks = 0;
-            totalInodes = 0;
-            freeList = 0;
-            //TODO: FINISH
+            //fix any missmatch
+            totalBlocks = diskSize;
+            totalInodes = DEFAULT_INODES;
+            freeList = totalInodes / INODES_PER_BLOCK + 1;
         }
-        //This function is to format the disk. Info for the superblock is
-        //located/stored in block 0
-        public synchronized void formatDisk(int numInodes)
-        {
-			// Each inode should be able to store at least one block of data
-			if (numInodes < 0 || numInodes > Disk.blockSize / 2 - 1)
-				return;
-		
-            byte[] block = new byte[Disk.blockSize];
-            totalInodes = numInodes;
-			
-            //get the pointer to the first free block, check for correct offset for reading
-            freeList = (numInodes % INODES_PER_BLOCK) == 0 ? 
-				numInodes / INODES_PER_BLOCK + 1 : numInodes / INODES_PER_BLOCK + 2;
-			
-            //write the stored data to the block data (block array)
-            SysLib.int2bytes(totalBlocks, block, 0);
-            SysLib.int2bytes(totalInodes, block, 4);
-            SysLib.int2bytes(freeList, block, 8);
-			SysLib.rawwrite(0, block);
-			
-			SysLib.cerr("\nFormatting inodes... ");
-			for (short i = 0; i < totalInodes; i++)
-			{
-				Inode inode = new Inode();
-				inode.toBlockData(block, i);
-			
-				if (i == INODES_PER_BLOCK || i + 1 == totalInodes)
-					SysLib.cwrite(i / INODES_PER_BLOCK + 1, block);
-			}
-			SysLib.cerr("Done.\n");
-			
+
+        SysLib.int2bytes(totalBlocks, block, 0);
+        SysLib.int2bytes(totalInodes, block, 4);
+        SysLib.int2bytes(freeList, block, 8);
+
+        SysLib.rawwrite(0, block);
+    }
+    /**
+     * used to invalidate current superblock information
+     */
+    public void invalidate() {
+        totalBlocks = 0;
+        totalInodes = 0;
+        freeList = 0;
+    }
+    /**
+     * This function is to format the disk. Info for the superblock is
+     * located/stored in block 0
+     * @param numInodes the # the system can keep track of
+     */
+    public synchronized void formatDisk(int numInodes) {
+        // Each inode should be able to store at least one block of data
+        if (numInodes < 0 || numInodes > Disk.blockSize / 2 - 1) {
+            return;
+        }
+
+        byte[] block = new byte[Disk.blockSize];
+        totalInodes = numInodes;
+
+        //get the pointer to the first free block, check for correct offset for reading
+        freeList = (numInodes % INODES_PER_BLOCK) == 0
+                ? numInodes / INODES_PER_BLOCK + 1 : numInodes / INODES_PER_BLOCK + 2;
+
+        //write the stored data to the block data (block array)
+        SysLib.int2bytes(totalBlocks, block, 0);
+        SysLib.int2bytes(totalInodes, block, 4);
+        SysLib.int2bytes(freeList, block, 8);
+        SysLib.rawwrite(0, block);
+
+        for (short i = 0; i < totalInodes; i++) {
+            Inode inode = new Inode();
+            inode.toBlockData(block, i);
+
+            if (i == INODES_PER_BLOCK || i + 1 == totalInodes) {
+                SysLib.cwrite(i / INODES_PER_BLOCK + 1, block);
+            }
+        }
+
 			//zero out all data inside the array, get rid of junk data
-			//or get rid of last times data
-			for(int index = 0; index < Disk.blockSize; index++)
-				block[index] = (byte)0;
-					
-			int tenth = totalBlocks / 10;
-					
-			SysLib.cerr("Formatting blocks ");
-            //Find the next free block for each block, until we have read all of the blocks
-            for(int currentBlock = freeList; currentBlock < totalBlocks; currentBlock++)
-            {
-				SysLib.int2bytes((currentBlock + 1 >= totalBlocks) ? -1 : currentBlock + 1, block, 0);
-					
+        //or get rid of last times data
+        for (int index = 0; index < Disk.blockSize; index++) {
+            block[index] = (byte) 0;
+        }
+
+        int tenth = totalBlocks / 10;
+
+        //Find the next free block for each block, until we have read all of the blocks
+        for (int currentBlock = freeList; currentBlock < totalBlocks; currentBlock++) {
+            SysLib.int2bytes((currentBlock + 1 >= totalBlocks) ? -1 : currentBlock + 1, block, 0);
+
                 //if at the end set the next free block we are invalid, other wise, set to next
-                //we will be saving this info as a short, not a int
-                SysLib.cwrite(currentBlock, block);
-				
-				if (currentBlock % tenth == 0)
-					SysLib.cerr(". ");
+            //we will be saving this info as a short, not a int
+            SysLib.cwrite(currentBlock, block);
+
+            if (currentBlock % tenth == 0) {
+                SysLib.cerr(". ");
             }
-			SysLib.cerr("Done.\n");
         }
-        
-        //save back to disk
-        public synchronized void sync()
-        {
-            byte[] data = new byte[Disk.blockSize];
-            //read in old data
-            SysLib.rawread(0, data);
-            //copy new information to store to a byte array
-            SysLib.int2bytes(totalBlocks, data, 0);
-            SysLib.int2bytes(totalInodes, data, 4);
-            SysLib.int2bytes(freeList, data, 8);
-			
+    }
+
+    /**
+     * save the superblock to disk
+     */
+    public synchronized void sync() {
+        byte[] data = new byte[Disk.blockSize];
+        //read in old data
+        SysLib.rawread(0, data);
+        //copy new information to store to a byte array
+        SysLib.int2bytes(totalBlocks, data, 0);
+        SysLib.int2bytes(totalInodes, data, 4);
+        SysLib.int2bytes(freeList, data, 8);
+
             //write the information in superblock to disk
-			//rawwrite instead of cwrite because we want the information
-			//to go directly to the disk.
-            SysLib.rawwrite(0,data);
-        }
-        public synchronized short getNextFreeBlock()
+        //rawwrite instead of cwrite because we want the information
+        //to go directly to the disk.
+        SysLib.rawwrite(0, data);
+    }
+    /**
+     * used to get the next free block when  it is needed
+     * @return the block number or error
+     */
+    public synchronized short getNextFreeBlock() {
+        short retVal = (short) freeList;
+         // indicates that there are no more free blocks
+        if (retVal == NO_MORE_BLOCKS)
         {
-            short retVal = (short) freeList;
-			if (retVal == NO_MORE_BLOCKS) // indicates that there are no more free blocks
-				return - 1;
-			
-            //grab the next free point in the blocks
-            byte[] data = new byte[Disk.blockSize];
-            //get the next free pointer from the one we are pointing at
-            SysLib.cread(freeList,data);
-			
-            //first thing in the block
-			short nextFree = (short) SysLib.bytes2int(data, 0);
-			
-			// Clear the block before giving it away.
-			for (int i = 0; i < Disk.blockSize; i++)
-				data[i] = 0;
-			SysLib.cwrite(freeList, data);
-			
-			// Next free will only equal 0 if the free list has not been fully
-			// allocated yet. Since no blocks past this block will be accessed 
-			// if this block is zeroed out, the next block is free.
-			//
-			// The primary motivation behind this methodolgy is that manually
-			// setting the entire free list on startup is quite time consuming,
-			// and is unnecessary if we assume that the disk is all 0s to start.
-			// This optimization is VERY implementation specific.
-			if (nextFree == 0)
-			{
-				if (nextFree >= totalInodes) // If we have allocated every block, make free list indicate this.
-					freeList = NO_MORE_BLOCKS;
-				else
-					freeList++;
-			}
-			else
-				freeList = nextFree;
-			
+            return - 1;
+        }
+
+        //grab the next free point in the blocks
+        byte[] data = new byte[Disk.blockSize];
+        //get the next free pointer from the one we are pointing at
+        SysLib.cread(freeList, data);
+
+        //first thing in the block
+        short nextFree = (short) SysLib.bytes2int(data, 0);
+
+        // Clear the block before giving it away.
+        for (int i = 0; i < Disk.blockSize; i++) {
+            data[i] = 0;
+        }
+        SysLib.cwrite(freeList, data);
+
+        // Next free will only equal 0 if the free list has not been fully
+        // allocated yet. Since no blocks past this block will be accessed 
+        // if this block is zeroed out, the next block is free.
+        //
+        // The primary motivation behind this methodolgy is that manually
+        // setting the entire free list on startup is quite time consuming,
+        // and is unnecessary if we assume that the disk is all 0s to start.
+        // This optimization is VERY implementation specific.
+        if (nextFree == 0) {
+            if (nextFree >= totalInodes) // If we have allocated every block, make free list indicate this.
+            {
+                freeList = NO_MORE_BLOCKS;
+            } else {
+                freeList++;
+            }
+        } else {
+            freeList = nextFree;
+        }
+
+        return retVal;
+    }
+    /**
+     * give a block back to the superblock
+     * @param blockNum the block to give back
+     * @return the number of the block freed or error
+     */
+    public int returnBlock(short blockNum) {
+        int retVal = -1;
+        //check validity
+        if (blockNum < 0 || blockNum > totalBlocks) {
             return retVal;
         }
-        //free given block
-        public int returnBlock(short blockNum)
-        {
-            int retVal = -1;
-            //check validity
-            if(blockNum < 0 || blockNum > totalBlocks)
-                return retVal;
-				
-            byte[] data = new byte[Disk.blockSize];
+
+        byte[] data = new byte[Disk.blockSize];
             //our new block is going to be the new end of the free list
-            //aka what the freelist will be pointing at
-            SysLib.int2bytes(freeList, data, 0);
-            //write the new data to the block
-            SysLib.cwrite(blockNum, data);
-            freeList = blockNum; // setting freelist to this block
-            return blockNum;
-        }
+        //aka what the freelist will be pointing at
+        SysLib.int2bytes(freeList, data, 0);
+        //write the new data to the block
+        SysLib.cwrite(blockNum, data);
+        freeList = blockNum; // setting freelist to this block
+        return blockNum;
+    }
 }
